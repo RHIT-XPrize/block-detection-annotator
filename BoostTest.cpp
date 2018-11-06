6e8ddfcae7a819cb37f07189aced37b19bdba70e
crowelmk <crowelmk@rose-hulman.edu>
2018-11-05 22:24:30 -0500

    Broken Boost build

---
+++ BoostTest.cpp
@@ -0,0 +1,12 @@
+#define BOOST_PYTHON_STATIC_LIB
+#include <boost/python.hpp>
+
+char const* SayHello()
+{
+	return "Hello, from c++ dll!";
+}
+BOOST_PYTHON_MODULE(HelloExt)
+{
+	using namespace boost::python;
+	def("SayHello", SayHello);
+}
---
+++ Boost_Python_Build/Boost_Python_Build.cpp
@@ -0,0 +1,16 @@
+﻿// Boost_Python_Build.cpp : Defines the entry point for the application.
+//
+
+char const* greet()
+{
+	return "hello, world";
+}
+
+#define BOOST_PYTHON_STATIC_LIB
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(hello)
+{
+	using namespace boost::python;
+	def("greet", greet);
+}
